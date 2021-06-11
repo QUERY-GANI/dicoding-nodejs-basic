2c58379c2c008c4b2a9adfd54c1280f02421137f
QUERY-GANI <ahmadxgani01@gmail.com>
2021-06-11 12:41:37 +0700

    nodejs readable stream part 2 Ref: https://www.dicoding.com/academies/261/tutorials/14652

--- stream/index.js
+++ stream/index.js
@@ -1,19 +1,31 @@
-const fs = require("fs");
-const path = require("path");
+// const fs = require("fs");
 
-const readableStream = fs.createReadStream(`${__dirname}/article.txt`, {
-    highWaterMark: 10
-});
+// const readableStream = fs.createReadStream(`${__dirname}/article.txt`, {
+//     highWaterMark: 10
+// });
  
+// readableStream.on("readable", () => {
+//     try {
+//         process.stdout.write(`[${readableStream.read()}]`);
+//     } catch(error) {
+//         // catch the error when the chunk cannot be read.
+//         console.log(error);
+//     }
+// });
+ 
+// readableStream.on("end", () => {
+//     console.log("Done");
+// });
+
+/**
+ * TODO:
+ * Buatlah program untuk membaca teks input.txt dan menuliskannya ulang pada berkas output.txt
+ * menggunakan teknik readable stream dan writable stream.
+ */
+
+const fs = require("fs")
+const readableStream = fs.createReadStream(`${__dirname}/input.txt`, { highWaterMark: 15 })
+const writableStream = fs.createWriteStream(`${__dirname}/output.txt`)
 readableStream.on("readable", () => {
-    try {
-        process.stdout.write(`[${readableStream.read()}]`);
-    } catch(error) {
-        // catch the error when the chunk cannot be read.
-        console.log(error);
-    }
-});
- 
-readableStream.on("end", () => {
-    console.log("Done");
-});
+    writableStream.write(`${readableStream.read()}\n`)
+})
